e25393e7677f016e5962de2446866e82f967b3b6
0eta0 <eta.develop@gmail.com>
2019-05-11 03:22:21 +0900

    added test

---
+++ README.md
@@ -0,0 +1,42 @@
+## markdown-it-pdf
+
+PDF viewer for markdown-it
+
+## Installation
+
+```
+npm install markdown-it-pdf
+```
+
+## Usage
+
+```js
+import markdownIt from "markdown-it";
+import markdownItPdf from "markdown-it-pdf";
+const md = new markdownIt("default", {
+  html: true
+});
+md.use(markdownItPdf);
+// You can also write like this.
+// md.use(require(markdown-it-pdf));
+const pdfUrl =
+  "https://raw.githubusercontent.com/0eta0/markdown-it-pdf/4103c6f583b5097cd3a429b8e67d7ffe882813f8/test.pdf";
+
+md.render(`@[pdf](${pdfUrl})`);
+```
+
+### Options
+```js
+md.use(require(markdown-it-pdf), {
+  showUrl: true
+});
+```
+
+If you add option "showUrl", The URL link will attach after PDF viewer.
+
+## Development
+
+### Test
+```
+npm test
+```
---
+++ test/index.test.js
@@ -0,0 +1,155 @@
+import markdownIt from "markdown-it";
+
+const md = new markdownIt("default", {
+  html: true
+});
+
+describe("Output Test With Valid URL", () => {
+  const pdfUrl =
+    "https://raw.githubusercontent.com/0eta0/markdown-it-pdf/4103c6f583b5097cd3a429b8e67d7ffe882813f8/test.pdf";
+
+  it("Should return PDF viewer and URL", () => {
+    const showUrl = true;
+    md.use(require("../"), {
+      showUrl: showUrl
+    });
+    expect(md.render(`@[pdf](${pdfUrl})`).trim()).toBe(
+      `<p><object data="${pdfUrl}" type="application/pdf" width="100%" height="100%" class="pdf-container"><iframe src="${pdfUrl}" width="100%" height="100%" style="border: none;">This browser does not support PDFs. Please download the PDF to view it: <a href="${pdfUrl}">Download PDF</a></iframe></object><a href="${pdfUrl}">${pdfUrl}</a></p>`.trim()
+    );
+  });
+
+  it("Should return only PDF viewer", () => {
+    const showUrl = false;
+    md.use(require("../"), {
+      showUrl: showUrl
+    });
+    expect(md.render(`@[pdf](${pdfUrl})`).trim()).toBe(
+      `<p><object data="${pdfUrl}" type="application/pdf" width="100%" height="100%" class="pdf-container"><iframe src="${pdfUrl}" width="100%" height="100%" style="border: none;">This browser does not support PDFs. Please download the PDF to view it: <a href="${pdfUrl}">Download PDF</a></iframe></object></p>`.trim()
+    );
+  });
+});
+
+describe("Output Test With Invalid URL 1", () => {
+  const pdfUrl =
+    "https://raw.githubusercontent.com/0eta0/markdown-it-pdf/4103c6f583b5097cd3a42967d7ffe882813f8/test";
+
+  it("Should return PDF viewer and URL", () => {
+    const showUrl = true;
+    md.use(require("../"), {
+      showUrl: showUrl
+    });
+    expect(md.render(`@[pdf](${pdfUrl})`).trim()).toBe(
+      `<p><object data="${pdfUrl}" type="application/pdf" width="100%" height="100%" class="pdf-container"><iframe src="${pdfUrl}" width="100%" height="100%" style="border: none;">This browser does not support PDFs. Please download the PDF to view it: <a href="${pdfUrl}">Download PDF</a></iframe></object><a href="${pdfUrl}">${pdfUrl}</a></p>`.trim()
+    );
+  });
+
+  it("Should return only PDF viewer", () => {
+    const showUrl = false;
+    md.use(require("../"), {
+      showUrl: showUrl
+    });
+    expect(md.render(`@[pdf](${pdfUrl})`).trim()).toBe(
+      `<p><object data="${pdfUrl}" type="application/pdf" width="100%" height="100%" class="pdf-container"><iframe src="${pdfUrl}" width="100%" height="100%" style="border: none;">This browser does not support PDFs. Please download the PDF to view it: <a href="${pdfUrl}">Download PDF</a></iframe></object></p>`.trim()
+    );
+  });
+});
+
+describe("Output Test With Invalid URL 2", () => {
+  const pdfUrl =
+    "https:/raw.githubusercontent/0eta0/markdown-it-pdf/4103c6f583b5097cd3a42967d7ffe882813f8/test.pdf";
+
+  it("Should return original markdown", () => {
+    const showUrl = true;
+    md.use(require("../"), {
+      showUrl: showUrl
+    });
+    expect(md.render(`@[pdf](${pdfUrl})`).trim()).toBe(
+      `<p>@[pdf](${pdfUrl})</p>`.trim()
+    );
+  });
+
+  it("Should return original markdown", () => {
+    const showUrl = false;
+    md.use(require("../"), {
+      showUrl: showUrl
+    });
+    expect(md.render(`@[pdf](${pdfUrl})`).trim()).toBe(
+      `<p>@[pdf](${pdfUrl})</p>`.trim()
+    );
+  });
+});
+
+describe("Output Test With Invalid Input 1", () => {
+  const pdfUrl =
+    "https://raw.githubusercontent.com/0eta0/markdown-it-pdf/4103c6f583b5097cd3a429b8e67d7ffe882813f8/test.pdf";
+
+  it("Should return markdown not enough ) at end", () => {
+    const showUrl = true;
+    md.use(require("../"), {
+      showUrl: showUrl
+    });
+    expect(md.render(`@[pdf](${pdfUrl}`).trim()).toBe(
+      `<p>@[pdf](${pdfUrl}</p>`.trim()
+    );
+  });
+
+  it("Should return markdown not enough ) at end", () => {
+    const showUrl = false;
+    md.use(require("../"), {
+      showUrl: showUrl
+    });
+    expect(md.render(`@[pdf](${pdfUrl}`).trim()).toBe(
+      `<p>@[pdf](${pdfUrl}</p>`.trim()
+    );
+  });
+});
+
+describe("Output Test With Invalid Input 2", () => {
+  const pdfUrl =
+    "https://raw.githubusercontent.com/0eta0/markdown-it-pdf/4103c6f583b5097cd3a429b8e67d7ffe882813f8/test.pdf";
+
+  it("Should return Link with @ at head", () => {
+    const showUrl = true;
+    md.use(require("../"), {
+      showUrl: showUrl
+    });
+    expect(md.render(`@[pf](${pdfUrl})`).trim()).toBe(
+      `<p>@<a href="${pdfUrl}">pf</a></p>`.trim()
+    );
+  });
+
+  it("Should return Link with @ at head", () => {
+    const showUrl = false;
+    md.use(require("../"), {
+      showUrl: showUrl
+    });
+    expect(md.render(`@[pf](${pdfUrl})`).trim()).toBe(
+      `<p>@<a href="${pdfUrl}">pf</a></p>`.trim()
+    );
+  });
+});
+
+describe("Output Test With Invalid Input 3", () => {
+  const pdfUrl =
+    "https://raw.githubusercontent.com/0eta0/markdown-it-pdf/4103c6f583b5097cd3a429b8e67d7ffe882813f8/test.pdf";
+
+  it("Should return general Link with pdf as title", () => {
+    const showUrl = true;
+    md.use(require("../"), {
+      showUrl: showUrl
+    });
+    expect(md.render(`[pdf](${pdfUrl})`).trim()).toBe(
+      `<p><a href="${pdfUrl}">pdf</a></p>`.trim()
+    );
+  });
+
+  it("Should return general Link with pdf as title", () => {
+    const showUrl = false;
+    md.use(require("../"), {
+      showUrl: showUrl
+    });
+    expect(md.render(`[pdf](${pdfUrl})`).trim()).toBe(
+      `<p><a href="${pdfUrl}">pdf</a></p>`.trim()
+    );
+  });
+});
